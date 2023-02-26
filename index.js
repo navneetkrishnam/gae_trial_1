8643de283c12e85cb899cd9303a657c90e10b2eb
M Navneet Krishna <navneetkrishnam@gmail.com>
2023-02-26 18:40:01 +0000

    init commit

---
+++ index.js
@@ -0,0 +1,13 @@
+const express = require('express');
+
+const app = express();
+
+const port = 8080;
+
+app.get('/', function (req, res) {
+	res.sendFile('index.html', {root: __dirname});
+});
+
+app.listen(port, () => {
+	console.log(`Now listening on port ${port}`);
+});
